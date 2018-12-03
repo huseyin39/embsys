3eb215f9f366e094eefaeb6a771bdcec8b85a560
huseyin39 <huseyin-39@hotmail.fr>
2018-12-03 14:18:52 +0100

    Testtt

--- labs/sysprog/gps_reader/reader.c
+++ labs/sysprog/gps_reader/reader.c
@@ -5,73 +5,117 @@
 #include <fcntl.h>
 #include <string.h>
 #include <termios.h>
+#include <time.h>
 
 #include <util.h>
 
 //-----------------------------------------------------------------------------
 int main(int argc, char *argv [])
 {
-    char * port = NULL;
+    char * port1 = NULL;
+    char * port2 = NULL;
+
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    printf ( "Current local time and date: %s", asctime (timeinfo) );
+
+    pid_t pid_father = getppid();
+    printf("Parent PID : %ld \n" , (long) pid_father) ;
 
     // parse comand line
-    if (argc != 3)
+    if (argc != 5)
     {
-        fprintf(stderr, "Invalid usage: reader -p port_name\n");
+        fprintf(stderr, "Invalid usage: reader -p port_name1 -q port_name2\n");
         exit(EXIT_FAILURE);
     }
 
-    char * options = "p:";
+    char * options = "p:q:";
     int option;
     while((option = getopt(argc, argv, options)) != -1)
     {
         switch(option)
         {
             case 'p':
-                port = optarg;
+                port1 = optarg;
                 break;
-
+	    case 'q':
+		port2 = optarg;
+                break;
             case '?':
                 fprintf(stderr, "Invalid option %c\n", optopt);
                 exit(EXIT_FAILURE);
         }
     }
 
-    // open serial port
-    int fd = open(port, O_RDWR | O_NOCTTY);
-    if (fd == -1)
+    // open serial ports
+    int fd1 = open(port1, O_RDWR | O_NOCTTY);
+    int fd2 = open(port2, O_RDWR | O_NOCTTY);
+
+    if (fd1 == -1 || fd2 == -1)
     {
         perror("open");
         exit(EXIT_FAILURE);
     }
-    tcflush(fd, TCIOFLUSH);
+    tcflush(fd1, TCIOFLUSH);
+    tcflush(fd2, TCIOFLUSH);
+
 
     // read port
-    char buff[50];
-    fd_set fdset;
+    char buff1[50];
+    fd_set fdset1;
+
 
     while(1)
     {
-        bzero(buff, sizeof(buff));
+        bzero(buff1, sizeof(buff1));
 
-        FD_ZERO(&fdset);
-        FD_SET(fd, &fdset);
+        FD_ZERO(&fdset1);
+        FD_SET(fd1, &fdset1);
 
-        select(fd+1, &fdset, NULL, NULL, NULL);
 
-        if (FD_ISSET(fd, &fdset))
+        select(fd1+1, &fdset1, NULL, NULL, NULL);
+
+
+        //read port1
+        if (FD_ISSET(fd1, &fdset1))
         {
-            int bytes = read (fd, buff, sizeof(buff));
+            int bytes = read (fd1, buff1, sizeof(buff1));
 
             if (bytes > 0)
             {
-                printf("%s\n", buff);
+                printf("Port 1: %s\n", buff1);
+                fflush(stdout);
+            }
+        }
+        bzero(buff1, sizeof(buff1));
+
+        FD_ZERO(&fdset1);
+        FD_SET(fd2, &fdset1);
+
+        select(fd2+1, &fdset1, NULL, NULL, NULL);
+	//read port2
+        if (FD_ISSET(fd2, &fdset1))
+        {
+            int bytes = read (fd2, buff1, sizeof(buff1));
+
+            if (bytes > 0)
+            {
+                printf("Port 2: %s\n", buff1);
                 fflush(stdout);
             }
         }
     }
 
-    // close serial port
-    close(fd);
+    // close serial ports
+    close(fd1);
+    close(fd2);
 
     exit(EXIT_SUCCESS);
 }
+
+	
+
+
